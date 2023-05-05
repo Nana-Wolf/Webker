4fac5d06d19c1c7a669af712c9e9cd747c5cd268
Farkas Naomi <h044558@stud.u-szeged.hu>
2023-05-05 23:30:07 +0200

    cart

--- src/app/pages/cart/cart.module.ts
+++ src/app/pages/cart/cart.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { CartComponent } from './cart.component';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { CartComponent } from './cart.component';
   ],
   imports: [
     CommonModule,
-    CartRoutingModule
+    CartRoutingModule,
+    MatCardModule
   ]
 })
 export class CartModule { }
